dccd813d86ccba53f84d13487ccfd3bac63635bc
Adam Ross <adamross202@gmail.com>
2024-03-25 09:55:04 -0600

    added chooseIdentity view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult chooseIdentity()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
