c9e51f2838434d2d11df037acf80f26f38ef1afe
mjbramm <mitchbrammer@gmail.com>
2024-03-25 09:54:06 -0600

    NewMB

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,11 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Darla.Models;
+//using Darla.Models;
 
 namespace Darla.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
 
     public IActionResult Index()
     {
