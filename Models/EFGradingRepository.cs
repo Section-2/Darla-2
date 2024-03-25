43487537037f029c5f5460cb2ee1dded2791381d
Mikayla Randall <mikayla.mae@icloud.com>
2024-03-25 09:55:54 -0600

    repository

---
+++ Models/EFGradingRepository.cs
@@ -0,0 +1,13 @@
+namespace Darla.Models;
+
+public class EFGradingRepository : IGradingRepository
+{
+    private GradingContext _context;
+
+    public EFGradingRepository(GradingContext temp)
+    {
+        _context = temp;
+    }
+
+    public IQueryable<Survey> Surveys => _context.Surveys;
+}
