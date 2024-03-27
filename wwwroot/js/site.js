d9c06aa1ad8986bbf24ff80b1aa442faadd208ca
tiffanyzang <120767467+tiffanyzang@users.noreply.github.com>
2024-03-27 09:36:42 -0600

    import css and js for judges

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,81 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+// #####--- this is the judges JavaScript Code ---##### //
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    let dragSrcEl = null;
+
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';
+
+        dragSrcEl = this;
+
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+
+        e.dataTransfer.dropEffect = 'move';
+
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+        this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation(); // stops the browser from redirecting.
+        }
+
+        if (dragSrcEl !== this) {
+            const draggedContent = e.dataTransfer.getData('text/html');
+
+            // Swap the HTML content between the dragged element and the target element
+            const targetContent = this.innerHTML;
+            this.innerHTML = dragSrcEl.innerHTML;
+            dragSrcEl.innerHTML = targetContent;
+
+            // Optionally, you might want to update other attributes or properties of the elements as well
+
+            // Clear the dragged content from the source element
+            e.dataTransfer.clearData();
+        }
+
+        return false;
+    }
+
+
+    function handleDragEnd(e) {
+        this.style.opacity = '1';
+
+        items.forEach(function (item) {
+            item.classList.remove('over');
+        });
+    }
+
+
+    let items = document.querySelectorAll('.container .box');
+    items.forEach(function(item) {
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragover', handleDragOver, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('drop', handleDrop, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+    });
+});
+
+// ####--- end Judges JavaScript ---##### //
