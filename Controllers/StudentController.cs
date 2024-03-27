71aa65bae49df9d32659ce66ab1ae302108466ae
Conway Hogan <conway2019hogan@gmail.com>
2024-03-27 09:20:01 -0600

    conflicts

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -6,17 +6,9 @@ namespace Darla.Controllers;
 
 class StudentController : Controller
 {
-<<<<<<< HEAD
-  
-
-    // public IActionResult Index()
-    // {
-        //return View();
-=======
     //public IActionResult Index()
     //{
     //    return View();
->>>>>>> ed5b48562a8a64be94cb039ba6d9a3684abe13d7
     //}
 
 }
