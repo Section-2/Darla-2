005e3346ae8190f8c719a73cb5dc38be9bb1eeb9
Tbsheff <92263166+Tbsheff@users.noreply.github.com>
2024-03-27 09:33:51 -0600

    commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -47,13 +47,8 @@ public class HomeController : Controller
         return View("Judge/ScheduleView");
     }
 
-    }
+    
     public IActionResult OpeningPage()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
     {
         return View();
     }
