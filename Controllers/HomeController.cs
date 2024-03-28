1391ec2429e3d0b2f12178fa2d388e43cce4d26a
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2024-03-27 19:16:40 -0600

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using Darla.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SQLitePCL;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
+// test 2
 namespace Darla.Controllers;
 
 public class HomeController : Controller
 {
+    private IIntexRepository _repo;
+    
+    public HomeController(IIntexRepository Repo)
+    {
+        _repo = Repo;
+    }
 
     public IActionResult Index()
     {
@@ -16,28 +26,80 @@ public class HomeController : Controller
     {
         return View();
     }
-    
+
+    public IActionResult AllGrades()
+    {
+        return View();
+    }
+    public IActionResult ClassRubric()
+    {
+        return View();
+    }
+    public IActionResult TaGradingProgress()
+    {
+        return View();
+    }
+
     public IActionResult JudgePage()
     {
         return View();
     }
 
+    [HttpGet]
     public IActionResult judge_survey()
     {
-        return View("Judge/judge_survey");
+        return View("Judge/judge_survey",new Presentation());
+    }
+
+    [HttpPost]
+    public IActionResult judge_survey(Presentation p)
+    {
+        if (ModelState.IsValid)
+        {
+            _repo.AddPresentationScore(p);
+        }
+
+        return RedirectToAction("JudgeDashboard", new Presentation());
     }
 
     // Action to open judge schedule
-    public IActionResult ScheduleView()
+    public IActionResult JudgeDashboard()
     {
-        return View("Judge/ScheduleView");
+        var roomSchedules = _repo.RoomSchedulesWithRooms;
+        return View("Judge/JudgeDashboard", roomSchedules);
     }
 
     public IActionResult OpeningPage()
     {
+        return View("Judge/JudgeDashboard");
+    }
+
+    public IActionResult RubricDetails()
+    {
+        return View();
+    }
+    //Allowing access to StudentSubmission
+        public IActionResult StudentProgress()
+    {
         return View();
     }
 
+//    public IActionResult ProfIndex()
+//    {
+//        ViewData["GradingProgress"] = 70;
+//        return View();
+//    }
+
+        public IActionResult CreateAccount()
+    {
+        return View();
+    }
+
+    public IActionResult ProfAddJudge()
+    {
+        return View();
+    }
+    public IActionResult ProfFullRubric()
     //Allowing access to StudentSubmission
     public IActionResult StudentProgress()
     {
@@ -49,5 +111,9 @@ public class HomeController : Controller
         ViewData["GradingProgress"] = 70;
         return View();
     }
-
+    public IActionResult ProfEditRubric()
+    {
+    var query = _context.Users.Where(x => x.PermissionType == 4);
+    return View();
+    }
 }
