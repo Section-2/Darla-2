acb7d021e8677bb940ed949c31d586ffa0371720
pochovillar <m.villar.barrios@gmail.com>
2024-04-01 08:43:32 -0600

    added js and my style sheet

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+(function ($) {
+    "use strict";
+
+    $(function () {
+        var header = $(".start-style");
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 10) {
+                header.removeClass('start-style').addClass("scroll-on");
+            } else {
+                header.removeClass("scroll-on").addClass('start-style');
+            }
+        });
+    });
+
+    //Animation
+
+    $(document).ready(function () {
+        $('body.hero-anime').removeClass('hero-anime');
+    });
+
+    //Menu On Hover
+
+    $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+        if ($(window).width() > 750) {
+            var _d = $(e.target).closest('.nav-item');
+            _d.addClass('show');
+            setTimeout(function () {
+                _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+            }, 1);
+        }
+    });
+
+})(jQuery);
