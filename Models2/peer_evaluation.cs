fac93c38fb869a5318e18a19d25147a547d7f3aa
Tbsheff <92263166+Tbsheff@users.noreply.github.com>
2024-04-02 22:51:56 -0600

    changed connection string

---
+++ Models2/peer_evaluation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Darla.Models2;
+
+public partial class peer_evaluation
+{
+    public int peer_evaluation_id { get; set; }
+
+    public string? evaluator_id { get; set; }
+
+    public string? subject_id { get; set; }
+
+    public int question_id { get; set; }
+
+    public int rating { get; set; }
+
+    public string? comments { get; set; }
+
+    public virtual student_team? evaluator { get; set; }
+
+    public virtual peer_evaluation_question question { get; set; } = null!;
+
+    public virtual student_team? subject { get; set; }
+}
---
+++ Models2/rubric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Darla.Models2;
+
+public partial class rubric
+{
+    public int assignment_id { get; set; }
+
+    public int class_code { get; set; }
+
+    public string? subcategory { get; set; }
+
+    public string? description { get; set; }
+
+    public int? max_points { get; set; }
+
+    public string? instructor_notes { get; set; }
+
+    public virtual ICollection<grade> grades { get; set; } = new List<grade>();
+}
